b81f85e1d72df5c555cb7dc0995256ea66e7f17b
Michał Rus <m@michalrus.com>
2013-07-23 17:01:30 +0200

    Add basic scrolling

--- custompaging/Views/ScrollView.m
+++ custompaging/Views/ScrollView.m
@@ -11,8 +11,9 @@
 @interface ScrollView ()
 
 @property (nonatomic,strong) NSMutableArray* pages;
+@property (nonatomic,strong) UIView* roll;
 
-@property (nonatomic) float touchStartY;
+@property (nonatomic) float touchPrevY;
 
 @end
 
@@ -22,39 +23,51 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.pages = [[NSMutableArray alloc] init];
+        self.roll = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 0)];
+        self.roll.backgroundColor = [UIColor blueColor];
+        [self addSubview:self.roll];
     }
     return self;
 }
 
 - (void) addPage:(UIView*)page {
     [self.pages addObject:page];
+    
+    float rollH = self.roll.bounds.size.height;
+    float rollW = self.roll.bounds.size.width;
+    
+    float w = page.bounds.size.width;
+    float h = page.bounds.size.height;
+    
+    page.frame = CGRectMake(0, rollH, w, h);
+    self.roll.frame = CGRectMake(0, 0, rollW, rollH + h);
+    [self.roll addSubview:page];
+}
+
+- (void) moveRollBy:(float)by {
+    float y = self.roll.frame.origin.y + by;
+    
+    y = MIN(y, 0);
+    y = MAX(y, -(self.roll.bounds.size.height - self.bounds.size.height));
+    
+    self.roll.frame = CGRectMake(0, y, self.roll.bounds.size.width, self.roll.bounds.size.height);
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
     
-    self.touchStartY = pos.y;
-    
-    NSLog(@"touch began at %f", pos.y);
-    [super touchesBegan:touches withEvent:event];
+    self.touchPrevY = pos.y;
 }
 
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
-    [super touchesCancelled:touches withEvent:event];
-}
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {}
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
-    
-    NSLog(@"touch ended at %f", pos.y);
-    [super touchesEnded:touches withEvent:event];
-}
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {}
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
     
-    NSLog(@"touch moved at %f", pos.y);
-    [super touchesMoved:touches withEvent:event];
+    [self moveRollBy:pos.y - self.touchPrevY];
+    self.touchPrevY = pos.y;
 }
 
 @end
