5ebf71e2fbc7ba865cf08d5de892936c8f905130
Michał Rus <m@michalrus.com>
2013-07-23 14:37:25 +0200

    Initialize with empty app

---
+++ custompaging/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  custompaging
+//
+//  Created by michalrus on 7/23/13.
+//  Copyright (c) 2013 michalrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
