067b9d2b1799bcdaa83528aa8f27f76c075a51ed
Michał Rus <m@michalrus.com>
2013-07-23 16:29:28 +0200

    Add touch event handling to ScrollView

--- custompaging/Views/ScrollView.m
+++ custompaging/Views/ScrollView.m
@@ -8,16 +8,53 @@
 
 #import "ScrollView.h"
 
+@interface ScrollView ()
+
+@property (nonatomic,strong) NSMutableArray* pages;
+
+@property (nonatomic) float touchStartY;
+
+@end
+
 @implementation ScrollView
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+        self.pages = [[NSMutableArray alloc] init];
     }
     return self;
 }
 
 - (void) addPage:(UIView*)page {
+    [self.pages addObject:page];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
+    
+    self.touchStartY = pos.y;
+    
+    NSLog(@"touch began at %f", pos.y);
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesCancelled:touches withEvent:event];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
+    
+    NSLog(@"touch ended at %f", pos.y);
+    [super touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint pos = [((UITouch*) [touches anyObject]) locationInView:self];
+    
+    NSLog(@"touch moved at %f", pos.y);
+    [super touchesMoved:touches withEvent:event];
 }
 
 @end
