d08262449b5a63e9a55fa1a07188b1a377df2051
Michał Rus <m@michalrus.com>
2013-07-23 17:45:31 +0200

    Add UIScrollView temporarily for UX comparison

--- custompaging/Controllers/ViewController.m
+++ custompaging/Controllers/ViewController.m
@@ -20,8 +20,10 @@
 {
     [super viewDidLoad];
     
-    float w = self.view.bounds.size.width;
+    float w = self.view.bounds.size.width / 2;
     float h = self.view.bounds.size.height;
+    
+    // custom ScrollView
     
     ScrollView* scrollView = [[ScrollView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
     [self.view addSubview:scrollView];
@@ -31,6 +33,20 @@
     [scrollView addPage:[UIView pageWithWidth:w height:h]];
     [scrollView addPage:[UIView pageWithWidth:w height:h*1.5]];
     [scrollView addPage:[UIView pageWithWidth:w height:h]];
+    
+    // UIScrollView
+    
+    UIScrollView* uiScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(w, 0, w, h)];
+    [self.view addSubview:uiScrollView];
+
+    float y = 0;
+    [uiScrollView addSubview:[UIView pageWithFrame:CGRectMake(0, y, w, h)]]; y += h;
+    [uiScrollView addSubview:[UIView pageWithFrame:CGRectMake(0, y, w, h)]]; y += h;
+    [uiScrollView addSubview:[UIView pageWithFrame:CGRectMake(0, y, w, h)]]; y += h;
+    [uiScrollView addSubview:[UIView pageWithFrame:CGRectMake(0, y, w, h * 1.5)]]; y += h * 1.5;
+    [uiScrollView addSubview:[UIView pageWithFrame:CGRectMake(0, y, w, h)]]; y += h;
+    [uiScrollView setContentSize:CGSizeMake(w, y)];
+//    uiScrollView.pagingEnabled = YES;
 }
 
 - (void)didReceiveMemoryWarning
