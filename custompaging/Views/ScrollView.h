d49f736d935917d906c78d3048903d2fa83d83f1
Michał Rus <m@michalrus.com>
2013-07-23 14:46:44 +0200

    Add empty ScrollView

---
+++ custompaging/Views/ScrollView.h
@@ -0,0 +1,13 @@
+//
+//  ScrollView.h
+//  custompaging
+//
+//  Created by michalrus on 7/23/13.
+//  Copyright (c) 2013 michalrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollView : UIView
+
+@end
---
+++ custompaging/Views/ScrollView.m
@@ -0,0 +1,21 @@
+//
+//  ScrollView.m
+//  custompaging
+//
+//  Created by michalrus on 7/23/13.
+//  Copyright (c) 2013 michalrus. All rights reserved.
+//
+
+#import "ScrollView.h"
+
+@implementation ScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+    }
+    return self;
+}
+
+@end
