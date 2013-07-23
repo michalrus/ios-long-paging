2605984aca406162fbf176545265d3691245bda2
Michał Rus <m@michalrus.com>
2013-07-23 14:44:27 +0200

    Add empty root ViewController

---
+++ custompaging/Controllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  custompaging
+//
+//  Created by michalrus on 7/23/13.
+//  Copyright (c) 2013 michalrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ custompaging/Controllers/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  custompaging
+//
+//  Created by michalrus on 7/23/13.
+//  Copyright (c) 2013 michalrus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
